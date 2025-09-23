66beb27ce5bbde7bfc4f647fc42b2337bfcde7b2
latteeea <kimtaeran4767@gmail.com>
2025-09-23 21:37:20 +0900

    design: 음악 리스트 개별 컴포넌트 구현

---
+++ app/src/main/java/com/killingpart/killingpoint/ui/screen/MainScreen/MusicListBox.kt
@@ -0,0 +1,16 @@
+package com.killingpart.killingpoint.ui.screen.MainScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.killingpart.killingpoint.data.model.Diary
+
+@Composable
+fun MusicListBox(diary: Diary?) {
+
+}
+
+@Preview
+@Composable
+fun MusicListPreview() {
+    MusicListBox(diary = null)
+}
