af9d5079c23779b1944bc6bfc9bd195495107a34
latteeea <kimtaeran4767@gmail.com>
2025-11-23 16:20:16 +0900

    feat: 안드로이드 기본 바텀바 hide 처리

--- app/src/main/java/com/killingpart/killingpoint/MainActivity.kt
+++ app/src/main/java/com/killingpart/killingpoint/MainActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import com.kakao.sdk.common.KakaoSdk
 import com.killingpart.killingpoint.navigation.NavGraph
@@ -15,6 +18,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
         enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             val navController = rememberNavController()
             NavGraph(
