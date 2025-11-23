af20ecb34579782fea216b5a96ad793a1af32f4a
latteeea <kimtaeran4767@gmail.com>
2025-11-23 21:05:03 +0900

    feat: 스플래시 화면간 애니메이션 적용 (수정 필요)

--- app/src/main/java/com/killingpart/killingpoint/ui/component/SplashScreen.kt
+++ app/src/main/java/com/killingpart/killingpoint/ui/component/SplashScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.CacheDrawModifierNode
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -30,54 +31,31 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(onTimeout: () -> Unit) {
-    var startFadeOut by remember { mutableStateOf(false) }
-    
+    var stage by remember { mutableStateOf(0) }
+
     LaunchedEffect(Unit) {
-        // 로고 페이드인 대기
-        delay(800)
-        // 페이드아웃 시작
-        startFadeOut = true
-        // 페이드아웃 애니메이션 시간 대기 후 콜백 호출
         delay(400)
+        stage = 1
+        delay(600)
+        stage = 2
+        delay(800)
+        stage = 3
+        delay(800)
+        stage = 4
+        delay(800)
         onTimeout()
     }
-
-    // 로고 페이드인 애니메이션
-    val logoFadeIn = animateFloatAsState(
-        targetValue = 1f,
-        animationSpec = tween(
-            durationMillis = 800,
-            easing = LinearEasing
-        )
-    )
-    
-    // 전체 화면 페이드아웃 애니메이션
-    val fadeOut = animateFloatAsState(
-        targetValue = if (startFadeOut) 0f else 1f,
-        animationSpec = tween(
-            durationMillis = 400,
-            easing = LinearEasing
-        )
-    )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
-            .alpha(fadeOut.value),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(Color.Black),
+        horizontalAlignment = Alignment.Start
     ) {
         Spacer(modifier = Modifier.height(246.dp))
 
-        Image(
-            painter = painterResource(id = R.drawable.splash_logo),
-            contentDescription = "스플래시 로고",
-            modifier = Modifier
-                .size(width = 300.dp, height = 78.dp)
-                .alpha(logoFadeIn.value)
-        )
+        AnimatedStage(stage)
     }
-
 }
 
 @Preview
