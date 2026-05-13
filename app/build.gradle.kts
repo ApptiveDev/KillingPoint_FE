6e075b838c12085ea404be74653ba5e740816166
yeochaeeon <codjs2659@pusan.ac.kr>
2026-05-13 20:43:38 +0900

    chore: 버전 코드 및 버전 네임 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.killingpart.killingpoint"
         minSdk = 29
         targetSdk = 36
-        versionCode = 32
-        versionName = "2.2.3"
+        versionCode = 33
+        versionName = "2.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
