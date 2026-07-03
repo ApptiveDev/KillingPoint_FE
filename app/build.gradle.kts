9b014f855eeff6f34691038f7f9c2f1ea10b9fe0
latteeea <kimtaeran4767@gmail.com>
2026-07-03 14:54:25 +0900

    fix: 내 컬렉션 사진 저장 디자인 파인튜닝 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.killingpart.killingpoint"
         minSdk = 29
         targetSdk = 36
-        versionCode = 41
-        versionName = "2.3.2"
+        versionCode = 42
+        versionName = "2.3.3"
 
         buildConfigField("String", "AMPLITUDE_API_KEY", "\"fcb84a98b48f87f85e7112a1587976fd\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
